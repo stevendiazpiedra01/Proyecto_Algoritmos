6493ad9a703a6e1f1c400ac20ad1bfefe6e8ad8a
Brayan Steven Diaz <brayandiazpiedrahita@gmail.com>
2020-11-12 06:30:10 -0500

    Modificacion metodos de encriptacion

--- .venv/algoritmoDeEncriptacion.py
+++ .venv/algoritmoDeEncriptacion.py
@@ -9,6 +9,14 @@ ivHex = "0001020304050607"
 crypt.SetEncodedIV(ivHex,"hex")
 keyHex = "0001020304050607"
 crypt.SetEncodedKey(keyHex,"hex")
-encStr = "4759AE0081E64B5EEFD3A4E3D92C47777CDA37E8CBE374400B2F92548ED5F50D657887E5504E9D7F2B9B1EC375704841"
-decStr = crypt.DecryptStringENC(encStr)
-print (decStr)
+class claseEncriptado:
+    
+    def metodoEncriptacion (password):
+        encStr = crypt.EncryptStringENC(password)
+        return encStr
+
+    def metodoDesencriptacion (password):
+        decStr = crypt.DecryptStringENC(password)
+        return decStr
+
+
